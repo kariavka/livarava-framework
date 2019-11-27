3217de8a5867cc44b8dbf32c74bf9a691d14e222
kariavka <artem@kariavka.com>
2019-11-27 19:05:27 +0200

    [+] Added Me Service

--- config/environment.js
+++ config/environment.js
@@ -1,51 +1,79 @@
 'use strict';
 
-module.exports = function(environment) {
-  let ENV = {
-    modulePrefix: 'livarava-framework',
-    environment,
-    rootURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
-    },
+module.exports = function (environment) {
+    let ENV = {
+        modulePrefix: 'livarava-framework',
+        environment,
+        rootURL: '/',
+        locationType: 'auto',
+        EmberENV: {
+            FEATURES: {
+                // Here you can enable experimental features on an ember canary build
+                // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
+            },
+            EXTEND_PROTOTYPES: {
+                // Prevent Ember Data from overriding Date.parse.
+                Date: false
+            }
+        },
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+        APP: {
+            // Here you can pass flags/options to your application instance
+            // when it is created
+        },
+
+        api: {
+            host: '//www.livarava.com',
+            socketio: '//www.livarava.com',
+            namespace: 'api/v2',
+            path: '/api/v2',
+            token: null,
+        },
+    };
+
+    if (environment === 'development') {
+        // ENV.APP.LOG_RESOLVER = true;
+        // ENV.APP.LOG_ACTIVE_GENERATION = true;
+        // ENV.APP.LOG_TRANSITIONS = true;
+        // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+        // ENV.APP.LOG_VIEW_LOOKUPS = true;
     }
-  };
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+    if (environment === 'test') {
+        // Testem prefers this...
+        ENV.locationType = 'none';
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
+        // keep test console output quieter
+        ENV.APP.LOG_ACTIVE_GENERATION = false;
+        ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+        ENV.APP.rootElement = '#ember-testing';
+        ENV.APP.autoboot = false;
+    }
 
-    ENV.APP.rootElement = '#ember-testing';
-    ENV.APP.autoboot = false;
-  }
+    if (environment === 'local') {
+        ENV['api'] = {
+            host: '//local.livarava.com',
+            socketio: '//local.livarava.com',
+            namespace: 'api/v2',
+            path: '/api/v2',
+            token: null,
+        };
+    }
 
-  if (environment === 'production') {
-    // here you can enable a production-specific feature
-  }
+    if (environment === 'master') {
+        ENV['api'] = {
+            host: '//www.livarava.com',
+            socketio: '//www.livarava.com',
+            namespace: 'api/v2',
+            path: '/api/v2',
+            token: null,
+        };
+    }
 
-  return ENV;
+    if (environment === 'production') {
+        // here you can enable a production-specific feature
+    }
+
+    return ENV;
 };
